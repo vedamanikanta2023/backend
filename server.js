c1d33f5993d453efbc2d418dcb03c6f3ef482c9b
vedamanikanta <manikanta.vedam@gmail.com>
2025-08-30 11:26:18 +0530

    formatted the code

--- server.js
+++ server.js
@@ -8,55 +8,46 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Mysql@462065424",
-    database:"vs"
+  host: "localhost",
+  user: "root",
+  password: "Mysql@462065424",
+  database: "vs",
 });
 
-db.connect((err)=>{
-    if(err){
-        console.log("Error while connecting to DB",err,"error end")
-        return;
-    }
-    
-    console.log("Connected to DB");
+db.connect((err) => {
+  if (err) {
+    console.log("Error while connecting to DB", err, "error end");
+    return;
+  }
+  console.log("Connected to DB");
 });
 
-db.query(`select * from users`,(err,result)=>{
-        if(err){console.log("Error occured while retreiving Data from DB",err)
-        return;}
-        console.log("DB data",result);
-        // res.send(result)
-    });
-
-
-app.post("/login",(req,res)=>{
+app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   const sqlQuery = `SELECT * FROM users WHERE username = '${username}'`;
 
-   db.query(sqlQuery,(err,result)=>{
-        if (err) {
+  db.query(sqlQuery, (err, result) => {
+    if (err) {
       console.error("Error retrieving data from DB", err);
       return res.status(500).json({ message: "Internal server error" });
     }
-            console.log("result",result);
-            
- if (result.length === 0) {
+    console.log("result", result);
+
+    if (result.length === 0) {
       return res.status(401).json({ message: "Invalid username or password" });
     }
-        const user = result[0];
-res.send(user);
+    const user = result[0];
+    res.send(user);
 
-        console.log("askdlj",result);
-    }); 
-    // res.send("")
+    console.log("askdlj", result);
+  });
+  // res.send("")
 });
 
-app.get("",(req,res)=>{
-    res.send("Hello")
-})
-app.listen(3000,()=>{
-console.log("server running at 3000");
-})
+app.get("", (req, res) => {
+  res.send("Hello");
+});
+app.listen(3000, () => {
+  console.log("server running at 3000");
+});
