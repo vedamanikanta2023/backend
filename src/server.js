e38a0bae818cca61082a5aa2799874e9f01c63a7
vedamanikanta <manikanta.vedam@gmail.com>
2025-10-17 00:05:44 +0530

    removing the unnecessary apis

--- src/server.js
+++ src/server.js
@@ -100,45 +100,6 @@ app.get("", (req, res) => {
   res.send("Hello");
 });
 
-app.post("/user/generateToken", (req, res) => {
-    // Validate User Here
-    // Then generate JWT Token
-
-    let jwtSecretKey = process.env.JWT_SECRET_KEY;
-    let data = {
-        time: Date(),
-        userId: 12,
-    }
-
-    const token = jwt.sign(data, jwtSecretKey);
-
-    res.send(token);
-});
-
-app.get("/user/validateToken", (req, res) => {
-    // Tokens are generally passed in header of request
-    // Due to security reasons.
-
-    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
-    let jwtSecretKey = process.env.JWT_SECRET_KEY;
-    console.log(tokenHeaderKey,req.headers,"headers");
-    try {
-        const token = req.headers[tokenHeaderKey];
-
-        console.log(token,"token");
-        const verified = jwt.verify(token, jwtSecretKey);
-        if (verified) {
-            return res.send("Successfully Verified");
-        } else {
-            // Access Denied
-            return res.status(401).send("Acess Denied");
-        }
-    } catch (error) {
-        // Access Denied
-        return res.status(401).send(` Access Denied.. ${error}`);
-    }
-});
-
 const PORT = process.env.PORT || 3000;
 
 
