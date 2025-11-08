253f0bba3d0d74490851cd0ef84567107bb076ba
vedamanikanta <manikanta.vedam@gmail.com>
2025-11-08 23:45:51 +0530

    separating the Bearer from Bearer token

--- src/middlewares/jwtSrvice.js
+++ src/middlewares/jwtSrvice.js
@@ -1,42 +1,46 @@
-const jwt  = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
-const generateJWTToken= (req, res) => {
-    // Validate User Here
-    // Then generate JWT Token
+const generateJWTToken = (req, res) => {
+  // Validate User Here
+  // Then generate JWT Token
 
-    let jwtSecretKey = process.env.JWT_SECRET_KEY;
-    let data = {
-        time: Date(),
-        userId: 12,
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  let data = {
+    time: Date(),
+    userId: 12,
+  };
+
+  const token = jwt.sign(data, jwtSecretKey);
+
+  return token;
+};
+
+const authenticateUserWithToken = (req, res, next) => {
+  // Tokens are generally passed in header of request
+  // Due to security reasons.
+
+  let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  console.log(tokenHeaderKey, req.headers, "headers");
+  try {
+    const tokenWithBearer = req.headers[tokenHeaderKey];
+
+    // Ensure the header exists and starts with "Bearer"
+    const token = tokenWithBearer?.startsWith("Bearer ")
+      ? tokenWithBearer.split(" ")[1]
+      : null;
+    console.log(token, "token");
+    const verified = jwt.verify(token, jwtSecretKey);
+    if (verified) {
+      next();
+    } else {
+      // Access Denied
+      return res.status(401).send("Acess Denied");
     }
+  } catch (error) {
+    // Access Denied
+    return res.status(401).send(` Access Denied.. ${error}`);
+  }
+};
 
-    const token = jwt.sign(data, jwtSecretKey);
-
-    return token
-}
-
-const authenticateUserWithToken= (req, res,next) => {
-    // Tokens are generally passed in header of request
-    // Due to security reasons.
-
-    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
-    let jwtSecretKey = process.env.JWT_SECRET_KEY;
-    console.log(tokenHeaderKey,req.headers,"headers");
-    try {
-        const token = req.headers[tokenHeaderKey];
-
-        console.log(token,"token");
-        const verified = jwt.verify(token, jwtSecretKey);
-        if (verified) {
-            next();
-        } else {
-            // Access Denied
-            return res.status(401).send("Acess Denied");
-        }
-    } catch (error) {
-        // Access Denied
-        return res.status(401).send(` Access Denied.. ${error}`);
-    }
-}
-
-module.exports = {generateJWTToken,authenticateUserWithToken}
+module.exports = { generateJWTToken, authenticateUserWithToken };
